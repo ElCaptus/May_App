e76f3d1501400e55dc5812ef36a863664ac87849
El Captus <elxcaptus@gmail.com>
2022-05-26 15:33:14 -0300

    api config

--- src/routes/poke.routes.js
+++ src/routes/poke.routes.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const { route } = require('express/lib/router')
 const pokemons = require('../models/pokemons')
+const PokemonSchema = require('../models/pokemons')
 const router = express.Router()
 
 router.get('/',async (req,res)=>{
-    const pokes = await pokemons.find()
+    const pokes = await PokemonSchema.find()
     res.json({
         status: 200,
         msg: 'API Works',
@@ -14,9 +16,35 @@ router.get('/',async (req,res)=>{
     })
 })
 
+router.get('/:id', async (req,res)=>{
+    const pokemon = await PokemonSchema.findById(req.params.id)
+    res.json(pokemon);
+})
+
 router.post('/', async (req,res)=>{
-    console.log(req.body)
-    res.send(`Tremendo lo que me queres hacer ${req.body}`)
+    const {name, pict} = req.body
+    const newPokemon = new PokemonSchema({name,pict})
+    await newPokemon.save();
+    res.json({
+        status: "saved"
+    })
+})
+
+//si quisiera actualizar img o algo
+router.put('/:id',async (req,res)=>{
+    const updateObj = req.body
+    const oldData = await PokemonSchema.findById(req.params.id)
+    await PokemonSchema.findByIdAndUpdate(req.params.id, updateObj)
+    res.json({
+        status: "Pokemon Updated"
+    })
+})
+
+router.delete('/:id', async (req,res)=>{
+    await PokemonSchema.findByIdAndRemove(req.params.id)
+    res.json({
+        status: 'Pokemon removed'
+    })
 })
 
 module.exports = router
