d51671267ff24eeebd0cae8a00d2d0c1ecda7c82
El Captus <elxcaptus@gmail.com>
2022-05-29 16:36:19 -0300

    Project name and nav started

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -3,9 +3,20 @@ import React, { Component } from "react";
 class App extends Component{
     render(){
         return (
-            <h1>May App</h1>
+            <div>
+                <nav className="light-blue darken4">
+                    <div className="container">
+                        <a className="brand-logo" href="/"> Poke Memo</a>
+                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <li><a href="memo.html">Game</a></li>
+                            <li><a href="pokemons.html">Pokemons</a></li>
+                            <li><a href="info.html">Info</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         )
     }
 }
 
-export default App;
+export default App; 
