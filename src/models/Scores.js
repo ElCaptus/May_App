3a0bb9081530888a7bc1b0ebb690dbee50a52342
El Captus <elxcaptus@gmail.com>
2022-06-04 02:08:36 -0300

    Scores model and api configured

---
+++ src/models/Scores.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const ScoreSchema = new Schema({
+    name: { type: String, required: true},
+    errors_attached: { type: Number, required: true},
+    date: { type: Date, required: true}
+})
+
+module.exports = mongoose.model('Score', ScoreSchema)
---
+++ src/routes/score.routes.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const { status } = require('express/lib/response')
+const router = express.Router()
+const ScoreSchema = require('../models/Scores')
+
+router.get('/',async (req,res)=>{
+    const scores = await ScoreSchema.find()
+    res.json(scores)
+})
+
+router.post('/', async (req,res)=>{
+    const {name, errors_attached} = req.body
+    const date = new Date()
+    let status = 'saved'
+    const newScore = new ScoreSchema({name,errors_attached, date})
+    await newScore.save().catch(err =>{
+        console.error(err)
+        status = err
+    })
+    res.json({
+        status,
+        score:{
+            name,
+            errors_attached,
+            date: date.valueOf()
+        }
+    })
+})
+
+router.delete('/:id', async (req,res)=>{
+    await ScoreSchema.findByIdAndRemove(req.params.id)
+    res.json({
+        status: `Score ${req.params.id} Removed`
+    })
+})
+
+module.exports = router
