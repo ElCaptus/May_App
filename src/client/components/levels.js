a6b0f0fe598f0833355a1a4b70972fe9a58dc197
El Captus <elxcaptus@gmail.com>
2022-06-08 00:05:33 -0300

    Dificulty changed

--- src/client/components/levels.js
+++ src/client/components/levels.js
@@ -2,25 +2,25 @@ export default {
     easy:{
         name:'easy',
         size: 8,
-        attempts: 5,
+        attempts: 7,
         class: ''
     },
     medium:{
         name:'medium',
         size: 12,
-        attempts: 5,
+        attempts: 9,
         class: 'medium-table'
     },
     hard:{
         name:'hard',
         size: 15,
-        attempts: 5,
+        attempts: 10,
         class: 'hard-table'
     },
     insane:{
         name:'insane',
         size: 21,
-        attempts: 5,
+        attempts: 10,
         class: 'insane-table'
     }
 }
